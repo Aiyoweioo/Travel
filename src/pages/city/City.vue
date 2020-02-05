2ba532b53f852509ea9b4b916c057c0739866599
Olivia <15818002534@163.com>
2020-02-05 16:37:08 +0800

    'cityHeader'

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <city-header/>
+  </div>
+</template>
+
+<script>
+import CityHeader from './component/Header'
+import axios from 'axios'
+export default {
+  name: 'Home',
+  components: {
+    CityHeader
+  }
+}
+</script>
+<style>
+</style>
