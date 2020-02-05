2434287f8ace574c8a0134aecaa93ca7ff016510
Olivia <15818002534@163.com>
2020-02-05 17:05:14 +0800

    'city页面搜索框实现'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <city-header/>
+    <city-search/>
   </div>
 </template>
 
 <script>
 import CityHeader from './component/Header'
+import CitySearch from './component/Search'
 import axios from 'axios'
 export default {
   name: 'Home',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch
   }
 }
 </script>
